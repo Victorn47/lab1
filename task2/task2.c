5c512cc5f1f91c191062130c516b35129773e298
Victorn47 <91434133+Victorn47@users.noreply.github.com>
2021-10-04 16:51:24 +0300

    Add files via upload

---
+++ task2/task2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x)
+
+{
+
+	return ((x+2)/sqrt(2*x)-x/(sqrt(2*x)+2)+2/(x-sqrt(2*x)))*((sqrt(x)-sqrt(2))/(x+2));
+
+}
+
+void main(void)
+
+{
+
+	double x = 1;
+
+	printf("x = %.4lf\n", x);
+
+	printf("f = %.4lf\n\n", f(x));
+
+	printf("x = ");
+
+	scanf("%lf", &x);
+
+	printf("f = %.4lf", f(x));
+
+}
